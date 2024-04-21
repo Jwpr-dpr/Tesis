771a08c4a29893f7648b0627b675c1e50476aa30
Jwpr-dpr <jwprdpr@gmail.com>
2024-04-20 21:55:39 -0500

    Cambios en ondas planas, por ahora sigue el problema de cuando las permitividades son iguales, así que le voy trabajando al 2D mas bien

--- Photonics/PlaneWaves.py
+++ Photonics/PlaneWaves.py
@@ -1,6 +1,8 @@
 from PhotonicCristal import *
+from scipy.linalg import eigh
 
-def planeWaves1D(cristal:Cristal1D,numG:int):
+
+def planeWaves1D_dispersion(cristal:Cristal1D,numG:int):
     '''
     Argumentos:
     cristal(Cristal): Objeto de tipo cristal que representa el cristal fotónico 1D
@@ -17,8 +19,7 @@ def planeWaves1D(cristal:Cristal1D,numG:int):
     eps2 = cristal.B.e
 
     a = l1+l2
-    numG = 50
-
+    
     G  = np.arange(-numG* 2 * np.pi/a, numG * 2* np.pi/a, 2* np.pi/a)
     G1 = np.arange(-numG* 2 * np.pi/a, numG * 2* np.pi/a, 2* np.pi/a)
     chi = np.zeros((len(G),len(G1)),dtype = 'complex_')
@@ -28,8 +29,8 @@ def planeWaves1D(cristal:Cristal1D,numG:int):
             if(G[i] - G1[J]) == 0:
                 chi[i,J] = 1/(l1+l2) * (1/eps1 * l1 + 1/eps2*l2)
             else:
-                chi[i,J] = 1j /(l1+l2)/(G[i]-G1[J]) * (1/eps1 * np.exp(-1j * (G[i]-G1[J]) * l1) - 1) + 1/eps2 * (np.exp(-1j * (G[i]-G1[J]) * (l1+l2)) - np.exp(-1j * (G[i]-G1[J])*l1))
-    
+                chi[i,J] = 1j /(l1+l2)/(G[i]-G1[J]) * ((1/eps1 * (np.exp(-1j * (G[i]-G1[J]) * l1) - 1)) + (1/eps2 * (np.exp(-1j * (G[i]-G1[J]) * (l1+l2)) - np.exp(-1j * (G[i]-G1[J])*l1))))
+
     M = np.zeros((len(G),len(G1)),dtype = 'complex_')
     ks = np.linspace(-2*np.pi/a, 2*np.pi/a,100)
     Ks = list()
@@ -51,8 +52,77 @@ def planeWaves1D(cristal:Cristal1D,numG:int):
         
     return dispersion, points
 
-def planeWaves2D(cristal:Cristal2D,kpoints):
+def planeWaves1D_field(cristal:Cristal2D,numG):
     
+    l1 = cristal.A.a
+    l2 = cristal.B.a
+
+    eps1 = cristal.A.e
+    eps2 = cristal.B.e
+
+    a = l1+l2
+
+    G  = np.arange(-numG* 2 * np.pi/a, numG * 2* np.pi/a, 2* np.pi/a)
+    G1 = np.arange(-numG* 2 * np.pi/a, numG * 2* np.pi/a, 2* np.pi/a)
+    chi = np.zeros((len(G),len(G1)),dtype = 'complex_')
+
+    for i in range(len(G)):
+        for J in range(len(G1)):
+            if(G[i] - G1[J]) == 0:
+                chi[i,J] = 1/(l1+l2) * (1/eps1 * l1 + 1/eps2*l2)
+            else:
+                chi[i,J] = 1j /(l1+l2)/(G[i]-G1[J]) * ((1/eps1 * (np.exp(-1j * (G[i]-G1[J]) * l1) - 1)) + (1/eps2 * (np.exp(-1j * (G[i]-G1[J]) * (l1+l2)) - np.exp(-1j * (G[i]-G1[J])*l1))))
+
     
+    def calculate_field(x, a, numG, k, Vs, in1):
+        H = 0
+        countt = 0
+        for G in np.arange(-numG*2*np.pi/a, numG*2*np.pi/a, 2*np.pi/a):
+            countt += 1
+            H += Vs[countt,in1]*np.exp(1j*(k+G)*x)
+        return H
+
+    
+    k = np.pi/(15*a)
+    in1 = 20
+
+    countG1 = 1
+    countG = 1
+    M1 = np.zeros((2*numG+1, 2*numG+1), dtype=complex)
+
+    for G in np.arange(-numG*2*np.pi/a, numG*2*np.pi/a, 2*np.pi/a):
+        for G1 in np.arange(-numG*2*np.pi/a, numG*2*np.pi/a, 2*np.pi/a):
+            M1[countG1-1,countG-1] = chi[countG1-1,countG-1]*(k+G1)*(k+G)
+            countG += 1
+        countG1 += 1
+        countG = 1
+
+    countG1 = 1
+
+    eigvals, eigvecs = eigh(M1)
+    ind = np.argsort(eigvals)
+    Ds = eigvals[ind]
+    Vs = eigvecs[:,ind]
+
+    freq = np.sqrt(abs(Ds[in1-1]))*a/2/np.pi
+
+    div = 15
+    div1 = 1500
+    x = np.linspace(0, a, div1+1)
+    FieldE = np.abs(calculate_field(x, a, numG, k, Vs, in1))**2
+
+    alt = np.max(np.abs(FieldE))
+    x1 = np.linspace(0, alt, div+1)
+    y1 = l1*np.ones(div+1)
+
+    plt.figure(2)
+    plt.plot(y1, x1, linewidth=5)
+    plt.plot(x, FieldE, linewidth=2)
+    plt.xlabel('x(m)', fontsize=20)
+    plt.ylabel('E (V/m)', fontsize=20)
+    #plt.ylim([0, alt])
+    plt.xlim([0, a])
+    plt.show()
+
     return
         
